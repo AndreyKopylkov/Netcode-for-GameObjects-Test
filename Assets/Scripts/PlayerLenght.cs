603bac0af37cc3aabb0983e662e5869d62c33cb3
Andrey Kopylkov <andreykopylkov@gmail.com>
2023-06-05 20:52:25 +0600

    Player change lenght

---
+++ Assets/Scripts/PlayerLenght.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerLenght : NetworkBehaviour
+{
+    [SerializeField] private Tail _tailPrefab;
+    
+    public NetworkVariable<ushort> Lenght = new(1,
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Server);
+
+    private List<Tail> _tails;
+    private Transform _lastTail;
+    private Collider2D _collider2D;
+    
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        _tails = new List<Tail>();
+        _lastTail = transform;
+        _collider2D = GetComponent<Collider2D>();
+        if(!IsServer) 
+            Lenght.OnValueChanged += LengthChanged;
+    }
+    
+    [ContextMenu("Add Length")]
+    private void AddLength()
+    {
+        Lenght.Value += 1;
+        InstantiateTail();
+    }
+
+    private void LengthChanged(ushort previousValue, ushort newValue)
+    {
+        Debug.Log("LengthChanged Callback");
+        InstantiateTail();
+    }
+
+    private void InstantiateTail()
+    {
+        Tail newTail = Instantiate(_tailPrefab, transform.position, Quaternion.identity);
+        newTail.SpriteRenderer.sortingOrder = -Lenght.Value;
+        newTail.NetworkedOwner = transform;
+        newTail.FollowTransform = _lastTail;
+        _lastTail = newTail.transform;
+        Physics2D.IgnoreCollision(newTail.GetComponent<Collider2D>(), _collider2D);
+        
+        _tails.Add(newTail);
+    }
+}
---
+++ Assets/Scripts/Tail.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tail : MonoBehaviour
+{
+    public Transform FollowTransform;
+    public Transform NetworkedOwner;
+    public SpriteRenderer SpriteRenderer;
+    
+    [SerializeField] private float _delayTime = 0.1f;
+    [SerializeField] private float _distance = 0.3f;
+    [SerializeField] private float _moveStep = 10f;
+
+    private Vector3 _targetPosition;
+
+    private void Update()
+    {
+        _targetPosition = FollowTransform.position - FollowTransform.forward * _distance;
+        _targetPosition += (transform.position - _targetPosition) * _delayTime;
+        _targetPosition.z = 0f;
+
+        transform.position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * _moveStep);
+    }
+}
